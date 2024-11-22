86a0a9352bf9cb6989b25092f17f344896a3e676
Shivam Bhardwaj <kakashisensay25@gmail.com>
2024-11-22 19:12:24 +0530

    Web Dev

---
+++ Project 3/script.js
@@ -0,0 +1,9 @@
+// Function to open the sidebar
+function handleOpenMenu() {
+    document.getElementById('sidebar').style.width = '250px';
+}
+
+// Function to close the sidebar
+function handleCloseMenu() {
+    document.getElementById('sidebar').style.width = '0';
+}
