671dac5817728b8bf257ac96062e7ce2e723fed5
Shivam Bhardwaj <kakashisensay25@gmail.com>
2024-11-22 19:16:35 +0530

    Delete Project 3 directory

--- Project 3/script.js
+++
@@ -1,9 +0,0 @@
-// Function to open the sidebar
-function handleOpenMenu() {
-    document.getElementById('sidebar').style.width = '250px';
-}
-
-// Function to close the sidebar
-function handleCloseMenu() {
-    document.getElementById('sidebar').style.width = '0';
-}
